fe765e628f815b862d9119781c926c38ae70aef5
jbecker7326 <100980495+jbecker7326@users.noreply.github.com>
2023-03-31 15:37:49 -0400

    Update reviewmap.js
    Interactive dropdown filters input data 'business_reviews.csv' by metro to generate map.

--- visualization/reviewmap.js
+++ visualization/reviewmap.js
@@ -1,4 +1,4 @@
-// define the dimensions and margins for the chloropleth map
+// define the dimensions and margins for the map
 const margin = {top: 30, right: 30, bottom: 70, left: 70},
     width = 800 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
@@ -22,20 +22,34 @@ const tooltip = d3.select("#chloropleth")
 
 // initialize svg groups
 const legend = svgchloropleth.append("g").attr("id", "legend")
-const zipcodes = svgchloropleth.append("g").attr("id", "zipcodes")
+const map = svgchloropleth.append("g").attr("id", "map")
 
 // create color scheme
 const colorScheme = ["#ffffb2", "#fecc5c", "#fd8d3c", "#e31a1c"];
 
-// csv and json paths
-const pathToCSV = "businesses_reviews.csv";
+// top 10 categories
+const top10 = ['Restaurants', 'Food', 'Shopping', 'Home Services', 'Beauty & Spas', 'Nightlife', 'Health & Medical', 'Local Services', 'Bars', 'Automotive']
 
+// csv and json paths
+const pathToCSV = "data\\businesses_reviews.csv";
 const pathsToJSON = [
-    {'state': 'PA', json: d3.json("pa_pennsylvania_zip_codes_geo.min.json")},
-    {'state': 'NJ', json: d3.json("nj_new_jersey_zip_codes_geo.min.json")},
-    {'state': 'DE', json: d3.json("de_delaware_zip_codes_geo.min.json")}
+    {'state': 'AZ', json: d3.json("data\\json\\az_arizona_zip_codes_geo.min.json")},
+    {'state': 'CA', json: d3.json("data\\json\\ca_california_zip_codes_geo.min.json")},
+    {'state': 'DE', json: d3.json("data\\json\\de_delaware_zip_codes_geo.min.json")},
+    {'state': 'FL', json: d3.json("data\\json\\fl_florida_zip_codes_geo.min.json")},
+    {'state': 'ID', json: d3.json("data\\json\\id_idaho_zip_codes_geo.min.json")},
+    {'state': 'IL', json: d3.json("data\\json\\il_illinois_zip_codes_geo.min.json")},
+    {'state': 'IN', json: d3.json("data\\json\\in_indiana_zip_codes_geo.min.json")},
+    {'state': 'LA', json: d3.json("data\\json\\la_louisiana_zip_codes_geo.min.json")},
+    {'state': 'MO', json: d3.json("data\\json\\mo_missouri_zip_codes_geo.min.json")},
+    {'state': 'NJ', json: d3.json("data\\json\\nj_new_jersey_zip_codes_geo.min.json")},
+    {'state': 'NV', json: d3.json("data\\json\\nv_nevada_zip_codes_geo.min.json")},
+    {'state': 'PA', json: d3.json("data\\json\\pa_pennsylvania_zip_codes_geo.min.json")},
+    {'state': 'TN', json: d3.json("data\\json\\tn_tennessee_zip_codes_geo.min.json")}
 ];
 
+// load review data
+console.log('loading data...')
 d3.dsv(",", pathToCSV, function (d) {
     return {
         zipcode: d.zipcode,
@@ -50,27 +64,30 @@ d3.dsv(",", pathToCSV, function (d) {
         fake_reviews: +d['fake_reviews']
     }
 }).then(function (d) {
-    // enter code to call ready() with required arguments
     ready(pathsToJSON, d)
 });
 
-// this function should be called once the data from files have been read
-// world: topojson from world_countries.json
-// gameData: data from ratings-by-country.csv
+// this function is called after the review data is read in
+// jsons: object containing states and paths to geojson files
+// reviewData: data from businesses_reviews.csv
 function ready(jsons, reviewData) {
-    // enter code to extract all unique metros from reviewData
+    console.log('ready!')
+
+    // extract all unique metros from reviewData
     let metros = [...new Set(reviewData.map(x => x.metro))].sort()
     metros = metros.filter(item => item !== '')
 
-    // enter code to append the metro options to the dropdown
+    // append the metro options to the dropdown
     const selectList = document.getElementById('metroDropdown');
-
     for (let i = 0; i < metros.length; i++) {
         let option = document.createElement('option');
         option.value = metros[i];
         option.text = metros[i];
         selectList.appendChild(option);
     }
+
+    // set default metro to philly for now
+    selectList.value = 'Philadelphia'
 
     // event listener for the dropdown. Update choropleth and legend when selection changes. Call createMapAndLegend() with required arguments.
     selectList.onchange = function () {
@@ -85,40 +102,58 @@ function ready(jsons, reviewData) {
 // this function should create a Choropleth and legend using the world and gameData arguments for a selectedGame
 // also use this function to update Choropleth and legend when a different game is selected from the dropdown
 function createMapAndLegend(jsons, reviewData, selectedMetro) {
-    // filter review data on metro
-    let businesses = reviewData.filter(function (d) {
-        return d.metro == selectedMetro
-    })
+    d3.select("#map").selectAll("*").remove();
+    console.log('creating map...')
 
-    // unique list of states by metro
-    let states = [...new Set(businesses.map(x => x.state))].sort()
+    // filter review data for metro
+    let businesses = reviewData.filter(d => d.metro == selectedMetro)
 
-    // filter jsons by state
-    //let filtered = jsons.filter(function (d) {
-    //    return states.includes(d.state)
-    //})
+    // unique list of states and zipcodes by metro
+    let states = [...new Set(businesses.map(d => d.state))].sort()
+    let zipcodes = [...new Set(businesses.map(d => d.zipcode))].sort()
 
-    let promises = []
-    for (let i = 0; i < states.length; i++) {
-        thisjson = jsons.filter(item => item.state == states[i])
-        promises.push(thisjson[0].json)
+    /*
+    // quantile scale for color
+    ratings = businesses.map(row => row.avgRating).sort(function (a, b) {return a - b});
+    let quantize = d3.scaleQuantile()
+        .domain(ratings)
+        .range(colorScheme)
+
+    // function for getting color for map
+    function getColor(zipcode) {
+        if (zipcodes.includes(zipcode)) {
+            let rating = businesses.find(obj => {return obj.zipcode === zipcode}).avgRating
+            return quantize(rating)
+        } else {
+            return ("grey")
+        }
     }
+     */
 
+    // filter on promises for states within metro area
+    let promises = []
+    states.forEach((state) => {
+        promises.push(jsons.filter(json => json.state == state)[0].json)
+    });
     Promise.all(promises).then( function (jsons) {
 
-        let resultObject = {};
-        Object.keys(jsons[0]).map(key => { // iterate over the keys
-          resultObject = {
-            ...resultObject,
-            [key]: [...jsons[0][key], ...jsons[1][key], ...jsons[2][key]] // merge two objects
-          }
-          return;
+        // append all for this metro area geojsons to one object
+        let allstates = {['features']: [], ['type']: 'FeatureCollection'};
+        Object.keys(jsons).map(key => {
+            allstates = {['features']: [...allstates['features'], ...jsons[key]['features']]}
         });
-        resultObject['type'] = 'FeatureCollection'
-        console.log(resultObject)
 
-            // initial projection and path for map
-        var center = d3.geoCentroid(resultObject)
+        // filter geojsons by zipcodes with data
+        let metroarea = {['features']: [], ['type']: 'FeatureCollection'};
+        allstates['features'].forEach(zip => { // iterate over the feature keys
+               if (zipcodes.includes(zip['properties']['ZCTA5CE10'])) {
+                   metroarea['features'].push(zip)
+                }
+        })
+
+        console.log('drawing...')
+        // initial projection and path for map
+        var center = d3.geoCentroid(metroarea)
         var scale  = 150;
         var offset = [width/2, height/2];
         var projection = d3.geoMercator().scale(scale).center(center).translate(offset);
@@ -126,7 +161,7 @@ function createMapAndLegend(jsons, reviewData, selectedMetro) {
 
         // using the path determine the bounds of the current map and use
         // these to determine better values for the scale and translation
-        var bounds  = path.bounds(resultObject);
+        var bounds  = path.bounds(metroarea);
         var hscale  = scale*width  / (bounds[1][0] - bounds[0][0]);
         var vscale  = scale*height / (bounds[1][1] - bounds[0][1]);
         var scale   = (hscale < vscale) ? hscale : vscale;
@@ -136,16 +171,35 @@ function createMapAndLegend(jsons, reviewData, selectedMetro) {
         projection = d3.geoMercator().center(center).scale(scale).translate(offset);
         path = path.projection(projection);
 
+        console.log(metroarea)
+        console.log(metroarea.features[0].properties.ZCTA5CE10)
+
         // create map
-        zipcodes.selectAll("path")
-            .data(resultObject.features)
+        map.selectAll("path")
+            .data(metroarea.features)
             .enter()
             .append("path")
-            //.attr("class", "continent")
+            .attr("class", "continent")
             .attr("d", path)
             //.attr("fill", function (d) {return getColor(d.properties.name);})
             .style("fill", "red")
             .attr("stroke", "black")
+            .on("mouseover", function (event, d) {
+                zipcode = d.properties.ZCTA5CE10
+                if (zipcodes.includes(zipcode)) {
+                    //let rating = businesses.find(obj => {return obj.zipcode === zipcode}).avgRating
+                    //let numUser = businesses.find(obj => {return obj.zipcode === zipcode}).numUsers
+                    //tooltip.html("Country: " + d.properties.name + "\n<br>Game: " + selectedGame + "<br>Rating: " + rating + "<br>Number of Users: " + numUser);
+                    tooltip.html("Zipcode: " + zipcode + "\n<br>Metro: " + selectedMetro + "<br>Rating: N/A <br>Number of Users: N/A");
+                } else {
+                    tooltip.html("Zipcode: " + zipcode + "\n<br>Metro: " + selectedMetro + "<br>Rating: N/A <br>Number of Users: N/A");
+                }
+                return tooltip.style("visibility", "visible");
+            })
+            .on("mousemove", function (event, d) {return tooltip.style("top", (event.pageY - 10) + "px").style("left", (event.pageX + 10) + "px");})
+            .on("mouseout", function (event, d) {return tooltip.style("visibility", "hidden");});
+
+        console.log('done drawing! select the next metro')
     }
     );
 
